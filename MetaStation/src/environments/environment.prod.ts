fc2cf2111890a166f06703adf899a1b8c1b836a1
guzulu <guzuludeva.alpharive@gmail.com>
2024-11-22 11:13:56 +0530

    updated-22.11.2024

---
+++ MetaStation/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  siteUrl: 'https://metastation.fi/',
+  baseUrl: 'https://metastation.fi/9zXVFYO0pjgqrjB/public/api',
+  tradeUrl: 'https://metastation.fi/9zXVFYO0pjgqrjB/public/',
+  websocketurl: 'ws://https://metastation.fi/:9090',
+  websocketchaturl: 'ws://https://metastation.fi/:9091',
+  pagination: 10,
+};
+
+// https://metastation.fi/
